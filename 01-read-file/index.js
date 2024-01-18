fdd4ec697a5d72e6111db4f3f2c2ef1d37759304
Anton <lipan48@gmail.com>
2024-01-18 14:58:58 +0300

    feat: read file without readstrem

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
-fs.readFile('./text.txt', 'utf8', (error, data) => {
+fs.readFile('./01-read-file/text.txt', 'utf8', (error, data) => {
   console.log(data);
+  error ? console.log(error) : null;
 });
