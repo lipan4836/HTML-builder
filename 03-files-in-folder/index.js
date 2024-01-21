2776080d64600e0d11bad4a74eb12aad0ec956df
Anton <lipan48@gmail.com>
2024-01-21 18:48:51 +0300

    feat: complete 3 part

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,20 @@
-const { log } = require('console');
 const fs = require('fs');
 const path = require('path');
 
 const folderPath = path.join(__dirname, 'secret-folder');
 
 fs.readdir(folderPath, { withFileTypes: true }, (error, files) => {
-  error ? console.log(error) : null;
+  error ? console.log(error.message) : null;
   files.forEach((file) => {
     if (file.isFile()) {
-      const filePath = path.join(__dirname, file.name);
-      const fileExtension = path.extname(filePath);
-      const fileName = path.basename(filePath, fileExtension);
+      const filePath = path.join(folderPath, file.name);
+      const fileName = path.parse(filePath).name;
+      const fileExtension = path.parse(filePath).ext.slice(1);
       fs.stat(filePath, (error, stats) => {
-        error ? console.log(error) : null;
-        console.log(`${fileName}-${fileExtension}-${stats.size}`);
+        error ? console.log(error.message) : null;
+        console.log(
+          `${fileName}-${fileExtension}-${(stats.size / 1024).toFixed(2)}kb`,
+        );
       });
     }
   });
