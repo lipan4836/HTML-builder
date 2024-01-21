d13f43f37cb49390609c0017bd0c1dc9cc5afe3f
Anton <lipan48@gmail.com>
2024-01-21 20:46:49 +0300

    feat: resume 4 part

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,9 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-const folderPath = path.join(__dirname, 'files');
-const folderPathCopy = path.join(__dirname, 'files-copy');
+async function copyFiles(folderOriginal, folderCopy) {
+  fs.readdir(folderOriginal, { withFileTypes: true }).then((files) => {
+    files.forEach((file) => {
+      const filePath = path.join(folderOriginal, file.name);
+      const filePathCopy = path.join(folderCopy, file.name);
+      if (file.isFile()) {
+        fs.copyFile(filePath, filePathCopy);
+      } else {
+        fs.mkdir(filePathCopy, { recursive: true });
+        copyFiles(filePath, filePathCopy);
+      }
+    });
+  });
+}
 
-function copyDir() {
-  
+async function copyDir() {
+  const folderPath = path.join(__dirname, 'files');
+  const folderPathCopy = path.join(__dirname, 'files-copy');
 }
