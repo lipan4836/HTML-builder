bde6ff2aff67817b3b7ff8d95b792a9dfaa51188
Anton <lipan48@gmail.com>
2024-01-21 19:16:31 +0300

    feat: start 4 part

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'files');
+const folderPathCopy = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+  
+}
