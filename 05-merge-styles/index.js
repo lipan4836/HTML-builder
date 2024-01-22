cd13d180e91b552d76797ad93230005645ad6989
Anton <lipan48@gmail.com>
2024-01-22 06:41:57 +0300

    feat: resume 5 part

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,4 +3,12 @@ const path = require('path');
 
 const stylesFolder = path.join(__dirname, 'styles');
 const stylesBundleFolder = path.join(__dirname, 'project-dist');
+
 const stylesBudleFile = path.join(stylesBundleFolder, 'bundle.css');
+const stream = fs.createWriteStream(stylesBudleFile);
+
+fs.readdir(stylesFolder, { withFileTypes: true }, (error, files) => {
+  error ? console.log(error.message) : null;
+
+  
+});
