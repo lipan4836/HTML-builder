a2d6dda87c8004e79dd69a1fa12176711261496a
Anton <lipan48@gmail.com>
2024-01-22 06:26:53 +0300

    feat: start 5 part

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
+const stylesFolder = path.join(__dirname, 'styles');
+const stylesBundleFolder = path.join(__dirname, 'project-dist');
+const stylesBudleFile = path.join(stylesBundleFolder, 'bundle.css');
