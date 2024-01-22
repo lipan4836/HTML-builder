f372c68d8c00c4626624591912406de3f6e59919
Anton <lipan48@gmail.com>
2024-01-22 17:16:39 +0300

    feat: resume 6 part

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -9,13 +9,21 @@ const assetsFolderPath = path.join(__dirname, 'assets');
 const assetsFolderCopyPath = path.join(folderPath, 'assets');
 const componentsFolderPath = path.join(__dirname, 'components');
 
+console.log('folderPath', folderPath);
+console.log('stylesFolderPath', stylesFolderPath);
+console.log('assetsFolderPath', assetsFolderPath);
+console.log('assetsFolderCopyPath', assetsFolderCopyPath);
+console.log('componentsFolderPath', componentsFolderPath);
+
 // main files
 const templatePath = path.join(__dirname, 'template.html');
-console.log(templatePath);
+console.log('templatePath', templatePath);
 
-const htmlFilePath = path.join(__dirname, 'index.html');
+const htmlFilePath = path.join(folderPath, 'index.html');
+console.log('htmlFilePath', htmlFilePath);
 
-const stylesFilePath = path.join(stylesFolderPath, 'style.css');
+const stylesFilePath = path.join(folderPath, 'style.css');
+console.log('stylesFilePath', stylesFilePath);
 
 // const readStream = fs.createReadStream(template, 'utf8');
 // readStream.on('data', (chunk) => console.log(chunk.toString()));
