9c1299e790fbe6431709e0614061cb78e0fb256d
Anton <lipan48@gmail.com>
2024-01-22 07:04:06 +0300

    feat: complete 5 part

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -10,5 +10,12 @@ const stream = fs.createWriteStream(stylesBudleFile);
 fs.readdir(stylesFolder, { withFileTypes: true }, (error, files) => {
   error ? console.log(error.message) : null;
 
-  
+  for (let file of files) {
+    if (path.parse(file.name).ext.slice(1) === 'css') {
+      const filePath = path.join(stylesFolder, file.name);
+
+      const readStream = fs.createReadStream(filePath);
+      readStream.pipe(stream);
+    }
+  }
 });
